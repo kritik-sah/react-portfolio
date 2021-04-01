90d64d9d3e9325f27bc7f4df8ae71d80aac54c60
kritik <kritiksah0@gmail.com>
2021-04-01 21:06:12 +0530

    About us v2

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
         <div className="about" id="about">
          <div className="sec-left ">
             <h4 className="txt-u-line">About Me:</h4>
-            <h1>I am a A
+            <h1>I am an A
             <Typed 
                 strings={[
                     '+ FullStack Web Developer',
@@ -23,13 +23,10 @@ const About = () => {
                     loop
             />
             </h1>
-            <p>There are so many ways i can describe my self but for now i think only One word is enough <strong>"<em className="txt-u-line">Learner</em>".</strong></p>
+            <p className="width-80">There are so many ways i can describe my self but for now i think only One word is enough <strong>"<em className="txt-u-line">Learner</em>".</strong></p>
             <a href="https://www.youtube.com/channel/UCh7-f_QrD7WcQcu62CixiHQ" target="_blank" rel="nofollow" className="btn m-10 btn-outline-danger"><Youtube /> YouTube</a>
             <a href="https://github.com/kritik-sah" target="_blank" rel="nofollow" className="btn m-10 btn-outline-warning"><Github /> GitHub</a>
             <a href="https://wa.link/xpwssg" target="_blank" rel="nofollow" className="btn m-10 btn-outline-success"><Whatsapp /> WhatsApp</a>
-            
-            
-
          </div>
          </div>
          <div className="luffy">
