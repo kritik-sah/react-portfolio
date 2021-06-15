e73795d4eb22b8e37d1783ed3e9ea4dbac8456c8
kritik <kritiksah0@gmail.com>
2021-06-16 05:01:07 +0530

    Domains + Infinite learning

--- src/data/PortfolioData.js
+++ src/data/PortfolioData.js
@@ -1,6 +1,30 @@
 import todoReact from "../img/proj/todo-react.gif";
 const PortfolioData = [
     {
+      cardName : '"ServicesStrategy.COM":Domain For Sale Landing Page',
+      cardDec : "I Bought This Amazing premium looking '.com' Domain and created a Informative and beautyful domain For Sale Landing page. plus listed on diffrent Aftermarkets to get free attention",
+      imgLink : "https://i.ibb.co/6DbBfmG/servicesstrategy1.png",
+      imgAlt : "ServicesStrategy.COM premium Domain Name for sale",
+      targateMsg : "Visit & BUY ServicesStrategy.COM",
+      targateLink : "https://ServicesStrategy.COM/",
+    },
+    {
+      cardName : '"PropertyNoble.COM":Domain For Sale Landing Page',
+      cardDec : "I Bought This Amazing premium looking '.com' Domain and created a Informative and beautyful domain For Sale Landing page. plus listed on diffrent Aftermarkets to get free attention",
+      imgLink : "https://i.ibb.co/HH0gRSv/propertynobel1.png",
+      imgAlt : "PropertyNoble.com premium Domain Name for sale",
+      targateMsg : "Visit & BUY PropertyNoble.com",
+      targateLink : "https://propertynoble.netlify.app/",
+    },
+    {
+      cardName : "INFINITE LEARNING! First Client",
+      cardDec : "Since every Business are Shutingdown bcoz of this Covid situation,So I am helping my client INFINITE LEARNING to migrate there existing Coaching Business online, Hosted on a VPS which is also maintained by me",
+      imgLink : "https://i.ibb.co/DMyCRtZ/infinitelearning-org-in.png",
+      imgAlt : "INFINITE LEARNING! LMS Created by IT Rebels-- Kritik Sah",
+      targateMsg : "Visit INFINITE LEARNING!",
+      targateLink : "https://infinitelearning.org.in/",
+    },
+    {
       cardName : "React.js ToDo App",
       cardDec : "A ToDo Application created with react.js, bootstrap 5 , Material UI and connected with firebase avilable on github and netlify",
       imgLink : todoReact,
